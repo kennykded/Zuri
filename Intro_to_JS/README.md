aa7ca26f00563ec171cea1d7f44cf49a3934df71
kennykded <82447341+kennykded@users.noreply.github.com>
2022-06-24 00:52:17 +0100

    Create a JavaScript page

---
+++ Intro_to_JS/README.md
@@ -0,0 +1,1 @@
+This is a Zuri Project which introduces Javascript.
---
+++ Intro_to_JS/script.js
@@ -0,0 +1,9 @@
+var name = "Kenny David";
+      console.log("My name is " + name);
+      alert("My name is " + name);
+      var height = "5'7";
+      console.log("I am " + height);
+      alert("I am " + height);
+      var country = "Nigeria";
+      console.log("The name of my country is " + country);
+      alert("The name of my country is " + country);
